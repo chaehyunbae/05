2b0298764e0e47035c5bbaa91d225f498c57a896
chaehyunbae <gotch9359@naver.com>
2020-10-06 16:51:34 +0900

    06

--- main.c
+++ main.c
@@ -5,20 +5,25 @@
 
 int main(int argc, char *argv[]) {
 	
-	int i,n;
-	int sum=0;
+	int answer = 59;
+	int i, trial=0;
 	
-	printf("input a number:");
-	scanf("%i", &n);
+	do
+	{
+		printf("input a  number:");
+		scanf("%i", &i);
+		if (answer >i)
+			printf("low!\n");
+		else if(answer <i)
+			printf("high!\n");
+		trial++;
+			
+
+	}while(i != answer);
 	
+	printf("conglaturation, number of trial : %i\n", trial);
 	
-	for (i=1;i<n;i++)
-		sum += i;
-		
-		
-	
-	printf("the sum result is %i\n", sum);
-	
+
 	
 	return 0;
 }
